02f85a6128ad0ac4193ba9db703b00da31d61f63
tunde-pegba <tundeshield@gmail.com>
2022-10-25 23:40:17 +0100

    Changed the method for printing. to complete on another branch.

--- src/components/qrcode/QrCode.js
+++ src/components/qrcode/QrCode.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 import { QrAlert } from '../Alerts';
 import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
 const QrCode = ({ id, qrCode }) => {
+  const handlePrint = () => {
+    window.print();
+  };
   return (
     <div class=" p-2 ml-14 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-6 dark:bg-gray-800 dark:border-gray-700">
       <span className="flex justify-between items-center">
         <h5 class="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
           Project QR Code
         </h5>
-        <Link to={`/tasks/create/${id}`}>
+        <span onClick={handlePrint}>
           <IconButton>
             <LocalPrintshopIcon />
           </IconButton>
-        </Link>
+        </span>
       </span>
 
       <p class="text-sm font-normal p-2 mb-2 text-gray-500 dark:text-gray-400">
